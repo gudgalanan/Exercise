d4d8619ec01a485917c6b3d69414a839bdee8009
gudgalanan <131745900+gudgalanan@users.noreply.github.com>
2023-10-19 23:14:03 +0700

    .

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,32 +1,15 @@
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-const schema = z.object({
-  title: z.string().min(3),
-  desc: z.string().min(3),
-  date: z.date().min(new Date(), { message: "Too old" }),
-  //   priority: z.enum("priority"),
-});
-
-type TaskFormData = z.infer<typeof schema>;
-
 const TaskForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<TaskFormData>({ resolver: zodResolver(schema) });
   return (
     <>
       <h1>New Task</h1>
       <form>
-        <input {...register("title")} type="text" id="title" />
+        <input type="text" id="title" />
         <label htmlFor="desc">
-          <textarea {...register("desc")} id="desc"></textarea>
+          <textarea id="desc"></textarea>
         </label>
         <div className="row">
           <label htmlFor="date">
-            <input type="date" id="date" {...register("date")} />
+            <input type="date" id="date" />
           </label>
           <label htmlFor="priority">
             <select id="priority">
